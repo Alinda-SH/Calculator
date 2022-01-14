278dd26a6f4568fd93b26a144ab89a6a57e30b36
Alinda-SH <70520287+Alinda-SH@users.noreply.github.com>
2022-01-14 13:45:01 +0100

    calculator strategy

---
+++ src/com/company/Addition.java
@@ -0,0 +1,10 @@
+package com.company;
+
+public class Addition implements Strategy{
+
+    @Override
+    public float calculation(float a, float b) {
+        return a+b;
+    }
+
+}
---
+++ src/com/company/Strategy.java
@@ -0,0 +1,8 @@
+package com.company;
+
+public interface Strategy {
+
+    public float calculation(float a, float b);
+}
+
+
